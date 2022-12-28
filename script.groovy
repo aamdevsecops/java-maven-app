f5bfe89ab4d85fdd19cd853cce680069564a7575
aamdevsecops <aamdevsecops@localhost>
2022-12-28 16:32:32 +0100

    added parameters as string

---
+++ script.groovy
@@ -0,0 +1,11 @@
+def buildJar() {
+    echo "building the application..."
+} 
+def buildImage()) {
+    echo "building the image"
+    
+    }
+def deployApp() {
+    echo "deploying the ${params.VERSION}"
+} 
+return this
